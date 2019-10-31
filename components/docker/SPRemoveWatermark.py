5bcc3abeeda19f4b2f0faee44bd215cc8f268e22
YanQinghao <woshiyanqinghao@gmail.com>
2019-10-31 22:11:04 +0800

    remove watermark

---
+++ components/docker/SPRemoveWatermark.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import numpy as np
+from suanpan.app import app
+from suanpan.storage import storage
+from arguments import Images
+
+
+@app.input(Images(key="inputImage"))
+@app.output(Images(key="outputImage"))
+def SPRemoveWatermark(context):
+    args = context.args
+    images = args.inputImage
+    alpha = 2.0
+    beta = -160
+
+    outputImages = []
+    for img in images:
+        new = alpha * img + beta
+        new = np.clip(new, 0, 255).astype(np.uint8)
+        outputImages.append(
+            (storage.delimiter.join(img.split(storage.delimiter)[8:]), new)
+        )
+
+    return outputImages
+
+
+if __name__ == "__main__":
+    SPRemoveWatermark()
+
