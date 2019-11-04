e52b0153ddcea4c8718270dfbaad20c4cfe093f5
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-04 11:48:34 +0800

    add log info

--- components/docker/SPViaSegment.py
+++ components/docker/SPViaSegment.py
@@ -5,6 +5,7 @@ import os
 import json
 from suanpan.app import app
 from suanpan.app.arguments import Folder
+from suanpan.log import logger
 from suanpan.utils import image
 from suanpan.storage import storage
 from arguments import Images
@@ -17,28 +18,36 @@ def SPViaSegment(context):
     args = context.args
     images = args.inputImage
 
-    jsonFile = os.path.join(args.inputData, "project.json")
-    with open(jsonFile, "rb") as load_f:
-        fileInfo = json.load(load_f)
-
     outputData = []
     files = []
-    for i, j in fileInfo["metadata"].items():
-        files.append(os.path.join(images.folder, j["vid"]))
-        try:
-            filename = os.path.splitext(j["vid"])[0] + "_" + j["av"]["1"] + ".png"
-        except:
-            filename = os.path.splitext(j["vid"])[0] + "_" + i.split("_")[-1] + ".png"
-        xy = j["xy"][1:]
-        img = image.read(os.path.join(images.folder, j["vid"]))
-        outputData.append(
-            (
-                filename,
-                img[
-                    int(xy[1]) : int(xy[1] + xy[3]), int(xy[0]) : int(xy[0] + xy[2]), :
-                ],
+
+    try:
+        jsonFile = os.path.join(args.inputData, "project.json")
+        with open(jsonFile, "rb") as load_f:
+            fileInfo = json.load(load_f)
+
+        for i, j in fileInfo["metadata"].items():
+            files.append(os.path.join(images.folder, j["vid"]))
+            try:
+                filename = os.path.splitext(j["vid"])[0] + "_" + j["av"]["1"] + ".png"
+            except:
+                filename = (
+                    os.path.splitext(j["vid"])[0] + "_" + i.split("_")[-1] + ".png"
+                )
+            xy = j["xy"][1:]
+            img = image.read(os.path.join(images.folder, j["vid"]))
+            outputData.append(
+                (
+                    filename,
+                    img[
+                        int(xy[1]) : int(xy[1] + xy[3]),
+                        int(xy[0]) : int(xy[0] + xy[2]),
+                        :,
+                    ],
+                )
             )
-        )
+    except:
+        logger.info("can not find project.json or json format error")
 
     for idx, img in enumerate(images):
         if images.images[idx] not in files:
