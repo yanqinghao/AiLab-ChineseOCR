4694ef6414c27e67612d2a7264eaeba3af6e9867
YanQinghao <woshiyanqinghao@gmail.com>
2019-12-05 14:32:57 +0800

    torch

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,7 @@
 docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker-gpu:$1 -f docker/docker_ocr_gpu/Dockerfile .
+sed -i "s/cpu/cu100/g" .dockerignore
 docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker:$1 -f docker/docker_ocr/Dockerfile .
+sed -i "s/cu100/cpu/g" .dockerignore
 
 docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker:$1
 docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker-gpu:$1
