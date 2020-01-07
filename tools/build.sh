6aa0dc9b004fbd7526ed439849e167f092c45113
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-07 15:05:19 +0800

    change namespace to shuzhi-amd64

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,11 @@
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker-gpu:$1 -f docker/docker_ocr_gpu/Dockerfile .
-sed -i "s/cpu/cu100/g" .dockerignore
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker:$1 -f docker/docker_ocr/Dockerfile .
-sed -i "s/cu100/cpu/g" .dockerignore
+NAMESPACE=("shuzhi-amd64")
+for i in ${NAMESPACE[*]}
+do
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/ocr-docker-gpu:$1 -f docker/docker_ocr_gpu/Dockerfile .
+    sed -i "s/cpu/cu100/g" .dockerignore
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/ocr-docker:$1 -f docker/docker_ocr/Dockerfile .
+    sed -i "s/cu100/cpu/g" .dockerignore
 
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/ocr-docker-gpu:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/ocr-docker:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/ocr-docker-gpu:$1
+done
